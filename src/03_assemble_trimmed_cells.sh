05d324652b495a1de04c34f0a82505244dd2b79f
diitaz93 <juan.sebastian.diaz.boada@ki.se>
2022-12-21 13:34:22 +0100

    MIssed the help function in 03.sh

--- src/03_assemble_trimmed_cells.sh
+++ src/03_assemble_trimmed_cells.sh
@@ -4,6 +4,31 @@
 # Author: Juan Sebastian Diaz Boada                                            #
 # Creation Date: 13/12/2022                                                    #
 # ============================================================================ #
+# Define the help function
+function help {
+  # Print the usage message
+  echo "Usage: $0 [INPUT_DIR][OUTPUT_DIR][NODES][LOCI]"
+  echo "Runs TraCeR over all trimmed fastq files."
+  echo ""
+  # Print a description of the script's parameters
+  echo "Parameters:"
+  echo "  INPUT_DIR     Relative path to the directory containing the trimmed fastq files."
+  echo "  OUTPUT_DIR    Relative path to the directory where the TCR files will be saved."
+  echo "  NODES         Number of nodes for TraCeR."
+  echo "  LOCI          'AB' for assembling alpha-beta chains and 'GD' for assembling gamma-delta chains."
+  echo ""
+  # Print the list of options
+  echo "Options:"
+  echo "  -h, --help        display this help and exit"
+  # Exit with a success status code
+  exit 0
+}
+
+# Parse the options and arguments
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  help
+fi
+
 INPUT_DIR=$1
 OUTPUT_DIR=$2
 NODES=$3
